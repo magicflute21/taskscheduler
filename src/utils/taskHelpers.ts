3c3f72e7826fc490d60bbb4b39b49b9ec678128b
Triin Lemba <triin.lemba@advectus.net>
2025-12-08 19:08:21 +0200

    Issues fix: 1. Add options to eachWeekOfInterval fn, to make monday as the first day of the week. 2. Task year - use getISOWeekYear, not plain getYear, to get the year where the task belongs to

--- src/utils/taskHelpers.ts
+++ src/utils/taskHelpers.ts
@@ -1,4 +1,4 @@
-import { eachWeekOfInterval, getISOWeek, getYear, parseISO } from "date-fns";
+import { eachWeekOfInterval, getISOWeek, getISOWeekYear, parseISO } from "date-fns";
 import type { Task } from "../stores/useTaskStore";
 
 const getTaskWeekNumber = (task: Task) => {
@@ -7,14 +7,18 @@ const getTaskWeekNumber = (task: Task) => {
   const start = parseISO(startDate);
   const end = parseISO(endDate);
 
-  const weeks = eachWeekOfInterval({ start, end });
+  const weeks = eachWeekOfInterval(
+    { start, end },
+    { weekStartsOn: 1 } // Monday. By default date-fns uses Sunday as the start of the week
+  );
+
   return weeks.map(week => getISOWeek(week));
 }
 
 const getTaskYear = (task: Task) => {
   const { startDate, endDate } = task;
-  const startYear = getYear(startDate);
-  const endYear = getYear(endDate);
+  const startYear = getISOWeekYear(startDate);
+  const endYear = getISOWeekYear(endDate);
 
   return [startYear, endYear];
 }
